bb64ec212d9febd5954b3d5ad37a2c1d77527f4e
Mariusz Jarząbek <mariusz@MacBook-Pro-Mariusz.local>
2017-10-12 13:15:49 +0200

    add first state

---
+++ app/pages/ToDoLists/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+export default class ToDoLists extends PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>todo</div>
+    );
+  }
+}
