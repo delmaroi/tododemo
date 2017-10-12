2e0b59ff29331c9348837dbe1a742722efa77886
Mariusz Jarząbek <mariusz@MacBook-Pro-Mariusz.local>
2017-10-12 13:23:22 +0200

    bootstrap Bootstrap

--- app/pages/ToDoLists/index.js
+++ app/pages/ToDoLists/index.js
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class ToDoLists extends PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>todo</div>
+      <Button>todo</Button>
     );
   }
 }
