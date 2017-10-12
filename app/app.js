1b5b8bad4908fb4b500a2f4423c4b2fe91103821
Mariusz Jarząbek <mariusz@MacBook-Pro-Mariusz.local>
2017-10-12 14:32:40 +0200

    cleanup

--- app/app.js
+++ app/app.js
@@ -14,20 +14,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter as Router } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import FontFaceObserver from 'fontfaceobserver';
 import { withRouter } from 'react-router-dom';
-import {
-  AuthenticationProvider,
-  sagas as authenticationSagas,
-  setConfig as reduxAuthSetConfig,
-} from 'redux-auth';
 import { sagas as apiSagas } from 'redux-connect-backend';
-import { sagas as tableContainerSagas } from 'react-table-container';
-
-import enUS from 'antd/lib/locale-provider/en_US';
-
-import chatSagas from 'react-chat/lib/sagas';
-import chatMessagesSagas from 'react-chat/lib/Message/sagas';
 
 import * as Config from 'Config'; // eslint-disable-line import/extensions
 import {
@@ -48,23 +36,10 @@ import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line impor
 /* eslint-enable import/no-webpack-loader-syntax */
 
 import configureStore from './store';
-import { translationMessages } from './i18n';
 import createRoutes from './routes';
 
 setBaseUrl(Config.API_URL);
 addTransformParamsFn(decamelizeOrderingParam);
-
-reduxAuthSetConfig({ camelizeUserDataKeys: true });
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
-// the index.html file and this observer)
-const openSansObserver = new FontFaceObserver('Open Sans', {});
-
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
-  document.body.classList.add('fontLoaded');
-}, () => {
-  document.body.classList.remove('fontLoaded');
-});
 
 // Create redux store with history
 // this uses the singleton browserHistory provided by react-router
@@ -76,15 +51,11 @@ const store = configureStore(initialState, history);
 
 apiSagas.map(store.runSaga);
 appSagas.map(store.runSaga);
-authenticationSagas.map(store.runSaga);
-chatMessagesSagas.map(store.runSaga);
-chatSagas.map(store.runSaga);
-tableContainerSagas.map(store.runSaga);
 
 const childRoutes = createRoutes(store);
 const AppWithRouter = withRouter(App);
 
-const render = (messages) => {
+const render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <Router history={history}>
@@ -97,35 +68,4 @@ const render = (messages) => {
   );
 };
 
-// Hot reloadable translation json files
-if (module.hot) {
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept('./i18n', () => {
-    render(translationMessages);
-  });
-}
-
-// Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  (new Promise((resolve) => {
-    resolve(import('intl'));
-  }))
-    .then(() => Promise.all([
-      import('intl/locale-data/jsonp/en.js'),
-      import('intl/locale-data/jsonp/de.js'),
-    ]))
-    .then(() => render(translationMessages))
-    .catch((err) => {
-      throw err;
-    });
-} else {
-  render(translationMessages);
-}
-
-// Install ServiceWorker and AppCache in the end since
-// it's not most important operation and if main code fails,
-// we do not want it installed
-if (process.env.NODE_ENV !== 'local') {
-  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
-}
+render();
